bf6df476f8fa9d9be8778cd1501bf38e0f2fd4d1
Emmuui <nikityuk.off@gmail.com>
2021-10-19 14:30:21 +0300

    random_repeat_song

--- gui_player.py
+++ gui_player.py
@@ -5,6 +5,7 @@ from tkinter import filedialog, ttk
 import time
 import pygame
 from mutagen.mp3 import MP3
+import random
 
 
 class App:
@@ -24,10 +25,10 @@ class App:
         self.cur_song = ''
         self.cur_time = 0
         self.song_volume = 0.5
-        self.playtime = None
         self.text = 'newest'
-        self.increment = True
-        self.id = 0
+        self.id = None
+        self.is_on = False
+        self.same_song_on = False
         self.frame_add_del_music()
         self.frame_song_list()
         self.frame_current_song()
@@ -50,6 +51,17 @@ class App:
         self.sort_by_date.add_command(label='By date', command=self.sort_by_newest_song)
         self.main_menu.add_cascade(label='Sort song', menu=self.sort_by_date)
 
+        image_repeat_song = PhotoImage(file=r'../mp3player/images/repeat_song.png')
+        self.button_repeat_song = Button(add_del_frame, image=image_repeat_song, bg='white')
+        self.button_repeat_song.image = image_repeat_song
+        self.button_repeat_song.bind('<Button-1>', self.same_song)
+        self.button_repeat_song.grid(row=0, column=2, padx=5)
+
+        image_random_song = PhotoImage(file=r'../mp3player/images/random_song.png')
+        self.button_random_song = Button(add_del_frame, image=image_random_song, bg='white')
+        self.button_random_song.image = image_random_song
+        self.button_random_song.bind('<Button-1>', self.switch_is_on)
+        self.button_random_song.grid(row=0, column=3, padx=5)
         add_del_frame.grid(row=0)
 
     # Frame for song list
@@ -70,9 +82,6 @@ class App:
         self.position_slider = ttk.Scale(current_song_frame, from_=0, to=100, orient=HORIZONTAL,
                                          value=0, length=300, command=self.song_info)
         self.position_slider.grid(row=1, column=0)
-
-        # self.slider_label = Label(current_song_frame, text='0')
-        # self.slider_label.grid(row=2, column=0)
         current_song_frame.grid(row=2, pady=5)
 
     # Frame for manipulate songs via buttons
@@ -123,11 +132,9 @@ class App:
         print('def song_to_play():')
         index = int(self.listbox.curselection()[0])
         self.cur_song = self.list_all_song[index]
-        song_mp3 = MP3(self.cur_song)
-        self.len_song = song_mp3.info.length
         self.label_cur_song['text'] = os.path.basename(self.cur_song).replace('.mp3', '')
         self.cur_time = 0
-        self.position_slider.config(to=int(self.len_song), value=int(self.cur_time))
+        self.position_slider.config(value=int(self.cur_time))
         self.func_play_song()
 
     # function for playing song
@@ -137,19 +144,49 @@ class App:
         pygame.mixer.music.set_volume(self.song_volume)
         pygame.mixer.music.load(song)
         pygame.mixer.music.play(start=0)
-
         self.track_play()
 
     def track_play(self):
-        print(f'def track_play(): {self.increment}')
+        print(f'self.is_on = {self.is_on} in track_play')
+        print(f'self.same_song_on = {self.same_song_on} in track_play')
+        if self.is_on:
+            self.button_random_song.config(bg='light grey')
+        else:
+            self.button_random_song.config(bg='white')
+
+        if self.same_song_on:
+            self.button_repeat_song.config(bg='light grey')
+        else:
+            self.button_repeat_song.config(bg='white')
+
+        song_mp3 = MP3(self.cur_song)
+        self.len_song = song_mp3.info.length
+        self.position_slider.config(to=int(self.len_song), value=int(self.cur_time))
+        print('def track_play():')
         self.cur_time += 1
         if int(self.cur_time) == int(self.len_song):
-            self.cur_time = 0
-            self.function_next_song()
+            if self.same_song_on:
+                self.label_cur_song['text'] = os.path.basename(self.cur_song).replace('.mp3', '')
+                self.cur_time = 0
+                self.position_slider.config(value=int(self.cur_time))
+                self.func_play_song()
+            elif not self.same_song_on:
+                if self.is_on:
+                    index = random.randint(0, len(self.list_all_song)-1)
+                    self.cur_song = self.list_all_song[index]
+                    self.label_cur_song['text'] = os.path.basename(self.cur_song).replace('.mp3', '')
+                    self.cur_time = 0
+                    self.position_slider.config(value=int(self.cur_time))
+                    self.func_play_song()
+                elif not self.is_on:
+                    self.cur_time = 0
+                    self.function_next_song()
         convert_len_song = time.strftime('%M:%S', time.gmtime(self.len_song))
         convert_cur_time = time.strftime('%M:%S', time.gmtime(self.cur_time))
         self.position_slider.config(to=int(self.len_song), value=int(self.cur_time))
         self.time_song.config(text=f'{convert_cur_time} / {convert_len_song}')
+        if self.id is not None:
+            self.root.after_cancel(self.id)
         self.id = self.root.after(1000, self.track_play)
 
     def song_info(self, x):
@@ -196,7 +233,6 @@ class App:
             index = 0
         self.cur_song = self.list_all_song[index]
         self.label_cur_song['text'] = os.path.basename(self.cur_song).replace('.mp3', '')
-        print(self.cur_song)
         self.func_play_song()
 
     def folder_song(self):
@@ -270,6 +306,25 @@ class App:
             pygame.mixer.music.set_volume(mute_volume)
             self.mute_unmute.config(image=self.image_mute)
             self.mute_unmute['text'] = 'mute'
+
+    def sort_by_name(self):
+        pass
+
+    def switch_is_on(self, x):
+        if not self.is_on:
+            self.is_on = True
+        else:
+            self.is_on = False
+
+        print(f'self.is_on = {self.is_on} in switch_is_on')
+
+    def same_song(self, x):
+        if not self.same_song_on:
+            self.same_song_on = True
+        else:
+            self.same_song_on = False
+
+        print(f'self.same_song_on = {self.same_song_on} in same_song')
 
 
 if __name__ == '__main__':
